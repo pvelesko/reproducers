71870f353f8813bea23e57d04368766c22732c77
Paulius Velesko <pvelesko@pglc.io>
2023-08-16 02:38:55 -0500

    trying to reproduce the blocking event query

--- ImmCmdListEventQuery/SlowKernel.cl
+++ ImmCmdListEventQuery/SlowKernel.cl
@@ -1,6 +1,6 @@
 __kernel void myKernel() {
     float val = 0.0f;
-    for (int i = 0; i < 10000; i++) {
+    for (int i = 0; i < 1000000; i++) {
         for (int j = 0; j < 10000; j++) {
             val += sqrt(val + i + j);
         }
--- ImmCmdListEventQuery/main.cpp
+++ ImmCmdListEventQuery/main.cpp
@@ -11,7 +11,7 @@
 #include <thread>
 #include <vector>
 
-// #define IMMEDIATE
+#define IMMEDIATE
 #include "common.hpp"
 #include "ze_api.h"
 
@@ -43,16 +43,19 @@ int main(int argc, char **argv) {
   };
 
   ze_event_handle_t StartEvent, EndEvent, timestampRecordEventStart,
-      timestampRecordEventStop, myEvent;
+      timestampMemcopyEventStart, myEvent;
   ZE_CHECK(zeEventCreate(EventPool_, &EventDesc, &StartEvent));
   EventDesc.index++;
   ZE_CHECK(zeEventCreate(EventPool_, &EventDesc, &EndEvent));
   EventDesc.index++;
   ZE_CHECK(zeEventCreate(EventPool_, &EventDesc, &timestampRecordEventStart));
   EventDesc.index++;
-  ZE_CHECK(zeEventCreate(EventPool_, &EventDesc, &timestampRecordEventStop));
+  ZE_CHECK(zeEventCreate(EventPool_, &EventDesc, &timestampMemcopyEventStart));
   EventDesc.index++;
   ZE_CHECK(zeEventCreate(EventPool_, &EventDesc, &myEvent));
+
+  ZE_CHECK(zeEventHostReset(timestampMemcopyEventStart));
+  ZE_CHECK(zeEventHostReset(timestampMemcopyEventStart));
 
   ze_device_mem_alloc_desc_t deviceMemDesc = {
       ZE_STRUCTURE_TYPE_DEVICE_MEM_ALLOC_DESC};
@@ -64,58 +67,41 @@ int main(int argc, char **argv) {
 
   void *startTime = nullptr;
   void *endTime = nullptr;
+  uint64_t startTimestamp, endTimestamp;
   zeMemAllocDevice(context, &deviceMemDesc, sizeof(uint64_t), 1, device,
                    &startTime);
   zeMemAllocDevice(context, &deviceMemDesc, sizeof(uint64_t), 1, device,
                    &endTime);
-  //   zeMemAllocShared(context, &deviceMemDesc, &hostMemDesc, sizeof(uint64_t),
-  //   8, device,
-  //                    (void**)(&startTime));
-  //   zeMemAllocShared(context, &deviceMemDesc, &hostMemDesc, sizeof(uint64_t),
-  //   8, device,
-  //                    (void**)(&endTime));
 
-
-  uint64_t hostTimestampStart, deviceTimestampStart;
-  uint64_t hostTimestampStop, deviceTimestampStop;
-  zeDeviceGetGlobalTimestamps(device, &hostTimestampStart,
-                              &deviceTimestampStart);
 
   // get the start time for the host
   auto start = std::chrono::steady_clock::now();
 
   zeCommandListAppendWriteGlobalTimestamp(
       cmdList, (uint64_t *)startTime, timestampRecordEventStart, 0, nullptr);
+    zeCommandListAppendBarrier(cmdList, nullptr, 0, nullptr);
+    zeCommandListAppendMemoryCopy(cmdList, &startTimestamp, startTime, sizeof(uint64_t),
+                                  timestampMemcopyEventStart, 0, nullptr);
+    zeCommandListAppendBarrier(cmdList, myEvent, 0, nullptr);
   std::cout << "Launching Kernel" << std::endl;
   // Launch kernel on the GPU
   ze_group_count_t dispatch;
   dispatch.groupCountX = 1;
   dispatch.groupCountY = 1;
   dispatch.groupCountZ = 1;
+  ze_kernel_indirect_access_flags_t flags = ZE_KERNEL_INDIRECT_ACCESS_FLAG_HOST | ZE_KERNEL_INDIRECT_ACCESS_FLAG_DEVICE;
+  zeKernelSetIndirectAccess(kernel, flags);
   ZE_CHECK(zeCommandListAppendLaunchKernel(cmdList, kernel, &dispatch, EndEvent,
                                            1, &timestampRecordEventStart));
+  execCmdList(cmdList);
   std::cout << "Kernel Launched" << std::endl;
-  zeCommandListAppendWriteGlobalTimestamp(
-      cmdList, (uint64_t *)endTime, timestampRecordEventStop, 1, &EndEvent);
-
-  // copy back the timestamp from device to host
-  uint64_t startTimeHost = 0, endTimeHost = 0;
-  // copy from device
-  zeCommandListAppendBarrier(cmdList, nullptr, 0, nullptr);
-  ZE_CHECK(zeCommandListAppendMemoryCopy(cmdList, &startTimeHost, startTime,
-                                         sizeof(startTime), nullptr, 0,
-                                         nullptr));
-  ZE_CHECK(zeCommandListAppendMemoryCopy(cmdList, &endTimeHost, endTime,
-                                         sizeof(endTime), nullptr, 0, nullptr));
-  zeCommandListAppendBarrier(cmdList, myEvent, 0, nullptr);
-
   // query StartEvent, then Event, then StartEvent, then Event
   //   ZE_CHECK(
   //       zeEventHostSynchronize(StartEvent,
   //       std::numeric_limits<uint64_t>::max()));
-  //   ze_result_t Status;
-  //   Status = zeEventQueryStatus(StartEvent);
-  //   std::cout << "StartEvent Query: " << resultToString(Status) << std::endl;
+    ze_result_t Status;
+    Status = zeEventQueryStatus(timestampMemcopyEventStart);
+    std::cout << "StartEvent Query: " << resultToString(Status) << std::endl;
   //   Status = zeEventQueryStatus(timestampRecordEventStop);
   //   std::cout << "EndEvent Query: " << resultToString(Status) << std::endl;
   //   Status = zeEventQueryStatus(StartEvent);
@@ -123,51 +109,7 @@ int main(int argc, char **argv) {
   //   Status = zeEventQueryStatus(timestampRecordEventStop);
   //   std::cout << "EndEvent Query: " << resultToString(Status) << std::endl;
 
-  execCmdList(cmdList);
-  std::cout << "Host Synchronize ...";
-  ZE_CHECK(
-      zeEventHostSynchronize(myEvent, std::numeric_limits<uint64_t>::max()));
-  ZE_CHECK(zeEventHostSynchronize(timestampRecordEventStop,
-                                  std::numeric_limits<uint64_t>::max()));
-  auto end = std::chrono::steady_clock::now();
-  auto hostTimeInMs =
-      std::chrono::duration_cast<std::chrono::milliseconds>(end - start)
-          .count();
-  std::cout << " complete" << std::endl;
-
-  zeDeviceGetGlobalTimestamps(device, &hostTimestampStop, &deviceTimestampStop);
-  ze_kernel_timestamp_result_t res{};
-  ZE_CHECK(zeEventQueryKernelTimestamp(EndEvent, &res));
-
-  // print the time in seconds
-  std::cout << "std::chrono Host: " << hostTimeInMs << " ms" << std::endl;
-  std::cout << "zeDeviceGetGlobalTimestamps Host: "
-            << timestampToMs(hostTimestampStart, hostTimestampStop) << " ms"
-            << std::endl;
-  std::cout << "zeDeviceGetGlobalTimestamps Device: "
-            << timestampToMs(deviceTimestampStart, deviceTimestampStop) << " ms"
-            << std::endl;
-  std::cout << "zeEventQueryKernelTimestamp Context: "
-            << timestampToMsKernel(res.context.kernelStart,
-                                   res.context.kernelEnd)
-            << " ms" << std::endl;
-  std::cout << "zeEventQueryKernelTimestamp Global: "
-            << timestampToMsKernel(res.global.kernelStart, res.global.kernelEnd)
-            << " ms" << std::endl;
-
-  ze_device_properties_t devProperties = {ZE_STRUCTURE_TYPE_DEVICE_PROPERTIES};
-  zeDeviceGetProperties(device, &devProperties);
-
-  uint64_t timerResolution = devProperties.timerResolution;
-  float copyOutDuration =
-      ((endTimeHost - startTimeHost) * timerResolution) / 1000000.0;
-  std::cout << "zeCommandListAppendWriteGlobalTimestamp Device: "
-            << copyOutDuration << " ms" << std::endl;
-
-  //   std::cout << "zeCommandListAppendWriteGlobalTimestamp Device: "
-  //             << timestampToMs(startTimeHost, endTimeHost) << " ms" <<
-  //             std::endl;
-
+ 
   cleanupLevelZero();
   return 0;
 }
